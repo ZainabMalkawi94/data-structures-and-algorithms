8f06e50ae7c704d3ce84bd907fbc7513e2781173
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-06-10 15:55:47 +0000

    Linked List

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,9 @@
 'use strict';
-
-class LinkedList {
-
-}
-
-module.exports = LinkedList;
+const linkedList = require('./lib/LinkedList');
+const LL = new linkedList();
+LL.insert("c");
+LL.insert("b");
+LL.insert("a");
+console.log(LL.toString());
+console.log(LL.includes("b"));
+console.log(LL.includes("d"));
